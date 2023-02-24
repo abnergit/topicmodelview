4f3e48d049a67b7939b55cc7456c62243cc738f6
abnergit <denysabner@gmail.com>
2023-02-23 23:14:38 -0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -14,8 +14,6 @@ require_once("conexao.php");
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
 
     <!-- Web Fonts -->
     <link href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
