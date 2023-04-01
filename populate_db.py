301dfa627a45615e910b442291ee84839c85b00e
abnergit <denysabner@gmail.com>
2023-04-01 09:35:50 -0300

    Update populate_db.py

--- populate_db.py
+++ populate_db.py
@@ -256,9 +256,11 @@ def write_terms(con, cur, terms_file):
     con.commit()
     id_autoincrement = 0
     for line in open(terms_file, 'r'):
-        cur.execute('INSERT INTO terms (id, title) VALUES(%s, %s)', [str(id_autoincrement), line.strip()])
-        id_autoincrement = id_autoincrement + 1
-
+        try:
+            cur.execute('INSERT INTO terms (id, title) VALUES(%s, %s)', [str(id_autoincrement), line.strip()])
+        except:
+            print(f"Nao adicionada ao BANCO: {line}")
+	id_autoincrement = id_autoincrement + 1
     con.commit()
 
 def write_docs(con, cur, docs_file):
